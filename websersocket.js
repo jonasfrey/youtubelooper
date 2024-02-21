4e71f566f0e2dc6632102c15341d4ce3890235e5
Your Name <you@example.com>
2024-02-21 01:36:23 +0100

    test

--- websersocket.js
+++ websersocket.js
@@ -9,94 +9,13 @@ let s_path_folder_current = s_path_file_current.split('/').slice(0, -1).join('/'
 // console.log(s_path_folder_current)
 const b_deno_deploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
 
-class O_ws_client{
-    constructor(
-        s_uuidv4,
-        o_socket
-    ){
-        this.s_uuidv4 = s_uuidv4
-        this.o_socket = o_socket
-    }
-}
-let a_o_ws_client = []
-
-class O_msg {
-    constructor(
-        s_js, 
-        a_v_param
-    ){
-        this.s_js = s_js
-        this.a_v_param = a_v_param
-    }
-}
-
 
 let f_handler = async function(o_request){
     // important if the connection is secure (https),
     // the socket has to be opened with the wss:// protocol
     // from the client
     // for client: const socket = new WebSocket(`${window.location.protocol.replace('http', 'ws')}//${window.location.host}`);
-    if(o_request.headers.get('Upgrade') == 'websocket'){
 
-        const {
-            socket: o_socket,
-            response: o_response
-        } = Deno.upgradeWebSocket(o_request);
-        let o_ws_client = new O_ws_client(
-            crypto.randomUUID(),
-            o_socket
-        )
-        a_o_ws_client.push(o_ws_client);
-
-        o_socket.addEventListener("open", (o_e) => {
-            o_socket.send(JSON.stringify(
-                new O_msg(
-                    `
-                        o_state.s_uuidv4 = '${o_ws_client.s_uuidv4}';
-                        console.log(o_state)
-                    `, 
-                    []
-                )
-            ))
-            
-        });
-
-        o_socket.addEventListener("message", async (o_e) => {
-            let o_data = JSON.parse(o_e.data);
-            a_o_ws_client
-                // .filter(o=>o!=o_ws_client)
-                .forEach(o=>{
-                    o.o_socket.send(JSON.stringify(
-                        new O_msg(
-                            `
-                                o_state.a_o_msg.push(
-                                    {
-                                        s_uuidv4: '${o_ws_client.s_uuidv4}',
-                                        s_msg: \`${o_data.s_msg}\`, 
-                                        n_ts_ms: ${new Date().getTime()}
-                                    }
-                                )
-                                o_state?.o_js__a_o_msg?._f_render()
-                                window.setTimeout(
-                                    ()=>{
-                                        let o = document.querySelector('.a_o_msg');
-                                        o.scrollTo(0, o.scrollHeight)
-                                    },
-                                    100
-                                )
-                            `, 
-                            []
-                        )
-                    ))
-
-                })
-        });
-        o_socket.addEventListener("close", async (event) => {
-            a_o_ws_client.splice(a_o_ws_client.indexOf(o_ws_client), 1);
-        });
-
-        return o_response;
-    }
     let o_url = new URL(o_request.url);
     if(o_url.pathname == '/'){
         return new Response(
